3266c38086a98c4f4c926f0ca695373fbfb70ec2
Gregory Conan <conan@ohsu.edu>
2020-01-31 10:57:19 -0800

    Fixed tientong98's issue, improvements to abcd2bids.py
    - Added functionality to bash scripts to set $TMPDIR if it is empty
    - Improved/polished abcd2bids.py
    - Fixed bug in unpack_and_setup.sh where $ABCD2BIDS_DIR was the wrong relative path

--- src/bin/run_data_gatherer.sh
+++ src/bin/run_data_gatherer.sh
@@ -6,6 +6,18 @@
 #
 exe_name=$0
 exe_dir=`dirname "$0"`
+
+# Added 2020-01-29 to ensure that $TMPDIR is nonempty and points to a real path
+if [ "x$TMPDIR" = "x" ]; then
+    export TMPDIR=/tmp
+    if [ ! -d $TMPDIR ]; then
+        mkdir $TMPDIR;
+    fi
+fi 
+if [ ! -d $TMPDIR/$USER ]; then
+    mkdir $TMPDIR/$USER
+fi
+
 echo "------------------------------------------"
 if [ "x$1" = "x" ]; then
   echo Usage:
