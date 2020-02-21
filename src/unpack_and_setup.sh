55ad31b9396e6e392e5f9441c179ea7b9339b97a
Gregory Conan <conan@ohsu.edu>
2020-02-21 14:38:12 -0800

    Reverted some but not all changes in unpack_and_setup.sh

--- src/unpack_and_setup.sh
+++ src/unpack_and_setup.sh
@@ -1,4 +1,4 @@
-#! /bin/bash 
+#! /bin/bash
 
 # Given a subject ID, session, and tgz directory:
 #   1) Copy all tgzs to compute node's disk
@@ -15,10 +15,13 @@
 # run_order_fix.py (in this repo)
 # sefm_eval_and_json_editor.py (in this repo)
 
+# modified by Greg 2020-02-05 to run this script from any location
+ABCD2BIDS_DIR="$(readlink -f ${0}/../..)"
+ 
 # If output folder is given as a command line arg, get it; otherwise use
 # ./data as the default. Added by Greg 2019-06-06
 if [ "x$4" = "x" ]; then
-    ROOT_BIDSINPUT=./data
+    ROOT_BIDSINPUT=${ABCD2BIDS_DIR}/data
 else
     ROOT_BIDSINPUT=$4
 fi
@@ -26,7 +29,7 @@ fi
 # If temp files folder is given as a command line arg, get it; otherwise use
 # ./temp as the default. Added by Greg 2019-06-07
 if [ "x$5" = "x" ]; then
-    ScratchSpaceDir=./temp
+    ScratchSpaceDir=${ABCD2BIDS_DIR}/temp
 else
     ScratchSpaceDir=$5
 fi
@@ -83,12 +86,7 @@ done
 mkdir ${TempSubjectDir}/BIDS_unprocessed
 echo ${participant}
 echo `date`" :RUNNING dcm2bids"
-
-#ABCD2BIDS_DIR="$(dirname "$ROOT_BIDSINPUT")"
-ABCD2BIDS_DIR=${HOME}/Projects/abcd/Scripts/abcd-dicom2bids/
-ABCD2BIDS_CONF=${HOME}/Projects/abcd/Scripts/abcd-dicom2bids//abcd_dcm2bids.conf
-
-dcm2bids -d ${TempSubjectDir}/DCMs/${SUB} -p ${participant} -s ${session} -c ${ABCD2BIDS_CONF} -o ${TempSubjectDir}/BIDS_unprocessed --forceDcm2niix --clobber
+dcm2bids -d ${TempSubjectDir}/DCMs/${SUB} -p ${participant} -s ${session} -c ${ABCD2BIDS_DIR}/abcd_dcm2bids.conf -o ${TempSubjectDir}/BIDS_unprocessed --forceDcm2niix --clobber
 
 echo `date`" :CHECKING BIDS ORDERING OF EPIs"
 if [[ -e ${TempSubjectDir}/BIDS_unprocessed/${SUB}/${VISIT}/func ]]; then
@@ -106,8 +104,8 @@ fi
 # select best fieldmap and update sidecar jsons
 echo `date`" :RUNNING SEFM SELECTION AND EDITING SIDECAR JSONS"
 if [ -d ${TempSubjectDir}/BIDS_unprocessed/${SUB}/${VISIT}/fmap ]; then
-    cp ${ROOT_BIDSINPUT}/dataset_description.json ${TempSubjectDir}/BIDS_unprocessed/
-    ${ABCD2BIDS_DIR}/src/sefm_eval_and_json_editor.py ${TempSubjectDir}/BIDS_unprocessed/ ${FSL_DIR} ${MRE_DIR} --participant-label=${participant} --output_dir $ROOT_BIDSINPUT
+    cp ${ROOT_BIDSINPUT}/dataset_description.json ${TempSubjectDir}/BIDS_unprocessed
+    ${ABCD2BIDS_DIR}/src/sefm_eval_and_json_editor.py ${TempSubjectDir}/BIDS_unprocessed/${SUB} ${FSL_DIR} ${MRE_DIR} --participant-label=${participant} --output_dir $ROOT_BIDSINPUT
 fi
 
 rm ${TempSubjectDir}/BIDS_unprocessed/${SUB}/ses-baselineYear1Arm1/fmap/*dir-both* 2> /dev/null || true
