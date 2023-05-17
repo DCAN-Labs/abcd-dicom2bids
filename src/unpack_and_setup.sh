c64e276436fa15372da63259c955c8b2227733f9
Audrey Houghton <hough129@umn.edu>
2023-05-17 10:47:36 -0500

    adjustments to unpack_and_setup.sh - needs to be tested

--- src/unpack_and_setup.sh
+++ src/unpack_and_setup.sh
@@ -176,36 +176,26 @@ rm ${TempSubjectDir}/BIDS_unprocessed/${SUB}/${VISIT}/fmap/*dir-both* 2> /dev/nu
 
 # rename EventRelatedInformation
 srcdata_dir=${TempSubjectDir}/BIDS_unprocessed/sourcedata/${SUB}/${VISIT}/func
-if ls ${TempSubjectDir}/DCMs/${SUB}/${VISIT}/func/*EventRelatedInformation.txt > /dev/null 2>&1; then
+if ls ${TempSubjectDir}/DCMs/${SUB}/${VISIT}/func/*EventRelatedInformation.* > /dev/null 2>&1; then
     echo `date`" :COPY AND RENAME SOURCE DATA"
     mkdir -p ${srcdata_dir}
-    MID_evs=`ls ${TempSubjectDir}/DCMs/${SUB}/${VISIT}/func/*MID*EventRelatedInformation.txt 2>/dev/null`
-    SST_evs=`ls ${TempSubjectDir}/DCMs/${SUB}/${VISIT}/func/*SST*EventRelatedInformation.txt 2>/dev/null`
-    nBack_evs=`ls ${TempSubjectDir}/DCMs/${SUB}/${VISIT}/func/*nBack*EventRelatedInformation.txt 2>/dev/null`
-    echo ${MID_evs}
-    echo ${SST_evs}
-    echo ${nBack_evs}
-    if [ `echo ${MID_evs} | wc -w` -eq 2 ]; then
+
+    tasks=(MID SST nBack)
+
+    for task in ${tasks}; do
+        envs= `ls ${TempSubjectDir}/DCMs/${SUB}/${VISIT}/func/*${task}*EventRelatedInformation.*`
+        echo ${envs}
+
         i=1
-        for ev in ${MID_evs}; do
-            cp ${ev} ${srcdata_dir}/${SUB}_${VISIT}_task-MID_run-0${i}_bold_EventRelatedInformation.txt
+        for ev in ${evs}; do
+            if [[ "${envs}" == *"nBack"* ]]; then
+                task=nback
+            fi
+            extension="${filename##*.}"
+            cp ${ev} ${srcdata_dir}/${SUB}_${VISIT}_task-${task}_run-0${i}_bold_EventRelatedInformation${extension}
             ((i++))
         done
-    fi
-    if [ `echo ${SST_evs} | wc -w` -eq 2 ]; then
-        i=1
-        for ev in ${SST_evs}; do
-            cp ${ev} ${srcdata_dir}/${SUB}_${VISIT}_task-SST_run-0${i}_bold_EventRelatedInformation.txt
-            ((i++))
-        done
-    fi
-    if [ `echo ${nBack_evs} | wc -w` -eq 2 ]; then
-        i=1
-        for ev in ${nBack_evs}; do
-            cp ${ev} ${srcdata_dir}/${SUB}_${VISIT}_task-nback_run-0${i}_bold_EventRelatedInformation.txt
-            ((i++))
-        done
-    fi
+    done
 fi
 
 echo `date`" :COPYING BIDS DATA BACK: ${ROOT_BIDSINPUT}"
